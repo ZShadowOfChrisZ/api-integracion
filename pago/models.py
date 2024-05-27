196dcb74cb0c60ee3ed9f556b3fbf91265292823
KevinMartinezMella <iamkamm7@gmail.com>
2024-05-26 23:57:07 -0400

    readme

--- pago/models.py
+++ pago/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import User as Usuario
 # Create your models here.
 class Pago(models.Model):
     id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    total = models.IntegerField()
